0c7715df9a2cacc23f1bdcfc99ca2a87c0779ead
AVITAL <A0583295187@GMAIL.COM>
2023-09-09 21:48:25 +0300

    Initial code + Welcome functions

---
+++ Stage0/Program5239.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome5239();
+            Welcome5247();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome5247();
+        private static void Welcome5239()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
