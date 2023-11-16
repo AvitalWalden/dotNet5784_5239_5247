66aca9c1f86a84aadc41ddd417a4b06b272c35dd
Michal <M0583295188@GMAIL.COM>
2023-11-16 18:30:46 +0200

    stage 2 - m

--- DalFacade/DO/Exceptions.cs
+++ DalFacade/DO/Exceptions.cs
@@ -1,19 +1,19 @@
 ﻿namespace DO;
 
-[Serializable]
-public class DalDoesNotExistException : Exception
+[Serializable] 
+public class DalDoesNotExistException : Exception // Exception of object that does not exist.
 {
     public DalDoesNotExistException(string? message) : base(message) { }
 }
 
 [Serializable]
-public class DalAlreadyExistsException : Exception
+public class DalAlreadyExistsException : Exception // Exception of object that already exist.
 {
     public DalAlreadyExistsException(string? message) : base(message) { }
 }
 
 [Serializable]
-public class DalDeletionImpossible : Exception
+public class DalDeletionImpossible : Exception // Exception of object that can't not be deleted.
 {
     public DalDeletionImpossible(string? message) : base(message) { }
 }
