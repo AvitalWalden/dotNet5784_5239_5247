8d95f8feea3e10dc3c32e0c58bf6f7c7d20d6873
Michal <M0583295188@GMAIL.COM>
2023-11-22 17:03:10 +0200

    AM

--- DalFacade/DO/Exceptions.cs
+++ DalFacade/DO/Exceptions.cs
@@ -1,4 +1,6 @@
-﻿namespace DO;
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DO;
 
 [Serializable] 
 public class DalDoesNotExistException : Exception // Exception of object that does not exist.
@@ -16,4 +18,16 @@ public class DalAlreadyExistsException : Exception // Exception of object that a
 public class DalDeletionImpossible : Exception // Exception of object that can't not be deleted.
 {
     public DalDeletionImpossible(string? message) : base(message) { }
+}
+
+[Serializable]
+public class DalInvalidEnteredValue : Exception // Exception of Invalid entered value.
+{
+    public DalInvalidEnteredValue(string? message) : base(message) { }
+}
+
+[Serializable]
+public class DalDataListIsEmpty: Exception // Exception of The data list is empty.
+{
+    public DalDataListIsEmpty(string? message) : base(message) { }
 }
