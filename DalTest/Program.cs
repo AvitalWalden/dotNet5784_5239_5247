2199f23ca29f3145496e45cff0724e8efcb70d97
Michal <M0583295188@GMAIL.COM>
2023-11-27 19:37:42 +0200

    Stage2 final commit

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -9,7 +9,7 @@ namespace DalTest
 {
     internal class Program
     {
-        static readonly IDal s_dal = new DalList(); //stage 2
+        static readonly IDal s_dal = new DalList(); //stage 2.
 
         // The function create a new task.
         public static void CreateTask()
