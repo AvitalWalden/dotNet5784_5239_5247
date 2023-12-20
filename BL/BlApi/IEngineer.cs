c03263850447e711e4069c2fe91595058b630c86
AVITAL <A0583295187@GMAIL.COM>
2023-12-20 23:38:15 +0200

    stage 4 - 8

--- BL/BlApi/IEngineer.cs
+++ BL/BlApi/IEngineer.cs
@@ -2,9 +2,32 @@
 
 public interface IEngineer
 {
+    /// <summary>
+    ///  creates new engineer
+    /// </summary>
+    /// <param name="item">the new task to be added</param>
+    /// <returns></returns>
     public int Create(BO.Engineer item);
+    /// <summary>
+    /// returns engineer by given id
+    /// </summary>
+    /// <param name="id">id of engineer that should be returned</param>
+    /// <returns></returns>
     public BO.Engineer? Read(int id);
-    public IEnumerable<BO.Engineer?> ReadAll(Func<BO.Engineer, bool>? filter = null)
+    /// <summary>
+    /// returns all the engineers that pass the condition
+    /// </summary>
+    /// <param name="filter">the condition that the engineer need</param>
+    /// <returns>all the tasks that pass the condition</returns>
+    public IEnumerable<BO.Engineer?> ReadAll(Func<BO.Engineer, bool>? filter = null);
+    /// <summary>
+    /// Updates engineer details
+    /// </summary>
+    /// <param name="item">the detailes of the engineer</param>
     public void Update(BO.Engineer item);
+    /// <summary>
+    /// Deletes a engineer by its Id
+    /// </summary>
+    /// <param name="id">id of the engineer that should be deleted</param>
     public void Delete(int id);
 }
---
+++ BL/BlImplementation/EngineerImplementation.cs
@@ -0,0 +1,31 @@
+﻿using BlApi;
+namespace BlImplementation;
+
+internal class EngineerImplementation : IEngineer
+{
+    private DalApi.IDal _dal = DalApi.Factory.Get;
+    public int Create(BO.Engineer item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public BO.Engineer? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<BO.Engineer?> ReadAll(Func<BO.Engineer, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(BO.Engineer item)
+    {
+        throw new NotImplementedException();
+    }
+}
