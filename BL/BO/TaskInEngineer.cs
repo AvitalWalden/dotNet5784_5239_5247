af0cd366aac7d976b27d59e18a934949ed58d24e
AVITAL <A0583295187@GMAIL.COM>
2023-12-20 20:20:53 +0200

    TaskInEngineer

---
+++ BL/BO/TaskInEngineer.cs
@@ -0,0 +1,9 @@
+﻿namespace BO;
+
+public class TaskInEngineer
+{
+    public int Id { get; init; }
+    public required string Alias { get; set; }
+    public override string ToString() => this.ToStringProperty();
+
+}
