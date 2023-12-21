dfe5db19976ab19c40c143fdbcb41f5391eab9b7
AVITAL <A0583295187@GMAIL.COM>
2023-12-21 19:15:07 +0200

    EngineerImplementation avital

--- BL/BlImplementation/EngineerImplementation.cs
+++ BL/BlImplementation/EngineerImplementation.cs
@@ -4,9 +4,9 @@ namespace BlImplementation;
 internal class EngineerImplementation : IEngineer
 {
     private DalApi.IDal _dal = DalApi.Factory.Get;
-    public int Create(BO.Engineer item)
+    public int Create(BO.Engineer engineer)
     {
-        throw new NotImplementedException();
+          
     }
 
     public void Delete(int id)
@@ -16,16 +16,52 @@ internal class EngineerImplementation : IEngineer
 
     public BO.Engineer? Read(int id)
     {
-        throw new NotImplementedException();
+        DO.Engineer? doEngineer = _dal.Engineer.Read(id);
+        if (doEngineer == null)
+            throw new BO.BlDoesNotExistException($"Engineer with ID={id} does Not exist");
+
+        return new BO.Engineer()
+        {
+            Id = id,
+            Name = doEngineer.Name,
+            Email = doEngineer.Email,
+            Level = (BO.EngineerExperience)doEngineer.Level,
+            Cost = doEngineer.Cost,
+            //Task = (TaskInEngineer)doEngineer.Task,
+        };
     }
 
     public IEnumerable<BO.Engineer?> ReadAll(Func<BO.Engineer, bool>? filter = null)
     {
-        throw new NotImplementedException();
+        return (from DO.Engineer doEngineer in _dal.Engineer.ReadAll()
+                select new BO.Engineer
+                {
+                    Id = doEngineer.Id,
+                    Name = doEngineer.Name,
+                    Email = doEngineer.Email,
+                    Level = (BO.EngineerExperience)doEngineer.Level,
+                    Cost = doEngineer.Cost,
+                    //Task = (TaskInEngineer)doEngineer.Task,
+                });
     }
 
     public void Update(BO.Engineer item)
     {
         throw new NotImplementedException();
+    }
+
+    private void ValidateEngineerData(BO.Engineer engineer)
+    {
+        if (engineer.Id <= 0)
+            throw new ArgumentException("Engineer ID must be a positive number");
+
+        if (string.IsNullOrWhiteSpace(engineer.Name))
+            throw new ArgumentException("Engineer name cannot be empty or null");
+
+        if (engineer.Cost <= 0)
+            throw new ArgumentException("Engineer cost must be a positive number");
+
+        if (string.IsNullOrWhiteSpace(engineer.Email))
+            throw new ArgumentException("Engineer email cannot be empty or null");
     }
 }
