4eb5890e3a9b29ff20b1189f3627a720bd59887c
AVITAL <A0583295187@GMAIL.COM>
2024-01-14 12:37:23 +0200

    add or update engineer

--- PL/Converters.cs
+++ PL/Converters.cs
@@ -20,3 +20,16 @@ class ConvertIdToContent : IValueConverter
         throw new NotImplementedException();
     }
 }
+
+class ConvertIdToIsEnable : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (int)value == 0 ? true : false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
