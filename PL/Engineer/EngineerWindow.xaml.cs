c0a708abda77de01b9d370064657c659519cd52c
AVITAL <A0583295187@GMAIL.COM>
2024-01-21 17:33:44 +0200

    Normal tests for ID and COST

--- PL/Engineer/EngineerWindow.xaml.cs
+++ PL/Engineer/EngineerWindow.xaml.cs
@@ -114,5 +114,30 @@ namespace PL.Engineer
             }
 
         }
+
+        private void IdOrCost_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox? text = sender as TextBox;
+            if (text == null) return;
+            if (e == null) return;
+            //allow get out of the text box
+            if (e.Key == Key.Enter || e.Key == Key.Return || e.Key == Key.Tab)
+                return;
+            //allow list of system keys (add other key here if you want to allow)
+            if (e.Key == Key.Escape || e.Key == Key.Back || e.Key == Key.Delete ||
+            e.Key == Key.CapsLock || e.Key == Key.LeftShift || e.Key == Key.Home
+            || e.Key == Key.End || e.Key == Key.Insert || e.Key == Key.Down || e.Key == Key.Right)
+                return;
+            char c = (char)KeyInterop.VirtualKeyFromKey(e.Key);
+            //allow control system keys
+            if (Char.IsControl(c)) return;
+            //allow digits (without Shift or Alt)
+            if (Char.IsDigit(c))
+                if (!(Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightAlt)))
+                    return; //let this key be written inside the textbox
+                            //forbid letters and signs (#,$, %, ...)
+            e.Handled = true; //ignore this key. mark event as handled, will not be routed to other controls
+            return;
+        }
     }
 }
